d97720b0662ba63b98e39cd411a06dcf1dfb02a6
Raymond C. Borges Hink <borgesraymond@gmail.com>
2016-07-15 13:56:59 -0500

    Update hostCheck.sh
    Added ability to do for a list of specific hosts

--- hostCheck.sh
+++ hostCheck.sh
@@ -3,6 +3,13 @@
 
 subnet="192.168.48."
 
+#To do with an array of specific hosts instead
+'''
+sigrid=(191 194 250)
+for host in "${sigrid[@]}"
+'''
+
+#Start ping loop
 for host in {1..255}
 do
         if ping  -w 1 -c 1 $subnet$host &> /dev/null
